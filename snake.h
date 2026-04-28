e6c5cc5328b43ac46d38d6f581ba77132176d2fc
WW-WalterWhite <georgehuangtianjian@gmail.com>
2026-04-28 21:01:03 +0800

    Create snake.h

---
+++ snake.h
@@ -0,0 +1,54 @@
+#ifndef SNAKE_H
+#define SNAKE_H
+
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include <string>
+#include <list>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+using namespace std::chrono_literals;
+
+// Structures
+struct boardstruct {
+    char display;
+    int status; 
+};
+
+struct snakelike {
+    int cordx;
+    int cordy;
+    snakelike(int cordxe, int cordye) : cordx(cordxe), cordy(cordye) {}
+};
+
+// Global Variable Declarations
+extern char userput;
+extern char bor;
+extern char validput[5];
+extern int mode;
+extern char blocks[99];
+extern boardstruct biowaste[21][21];
+extern int head[2];
+extern list<snakelike> snakey;
+extern string fullboard;
+extern std::chrono::milliseconds paustime;
+extern int dirgate;
+extern bool loopin;
+extern int highscore;
+
+// Function Prototypes
+void loadScore();
+void saveScore(int current);
+bool deathdetect();
+void clearformat();
+void inputstyle();
+void pelletdrop();
+void setboard();
+void shootboard();
+void abalode();
+char getcha(); // Linux specific input
+
+#endif
